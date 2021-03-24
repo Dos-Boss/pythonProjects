586e24d0f258b614bcfe861d3ed382208150fb3d
Dos-Boss <64688605+Dos-Boss@users.noreply.github.com>
2021-03-24 20:24:49 +0800

    mac_changer.py
    Simple MAC Address changer

---
+++ mac_changer.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+# MAC address changer
+# Brendan McCann
+# 21/03/2021
+
+import subprocess
+import optparse
+import re
+
+
+def get_args():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help=("New MAC address"))
+    (options, _arguments) = parser.parse_args()
+
+    if not options.interface:
+        parser.error('Invalid interface, use --help for more info.')
+    elif not options.new_mac:
+        parser.error('Invalid MAC, use --help for more info.')
+    return options
+
+
+def changeMac(interface, new_mac):
+    print('[+] Changing MAC address for ' + interface + ' to ' + new_mac)
+    subprocess.call(['ifconfig', interface, 'down'])
+    subprocess.call(['ifconfig', interface, 'hw', 'ether', new_mac])
+    subprocess.call(['ifconfig', interface, 'up'])
+
+
+def getCurrentMac(interface):
+    ifconfig_result = subprocess.check_output(['ifconfig', interface])
+
+    mac_search_result = re.search(r'\w\w:\w\w:\w\w:\w\w:\w\w:\w\w', str(ifconfig_result))
+
+    if mac_search_result:
+        return mac_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address")
+
+
+options = get_args()
+currentMac = getCurrentMac(options.interface)
+print('Current MAC = ' + str(currentMac))
+
+changeMac(options.interface, options.new_mac)
+
+currentMac = getCurrentMac(options.interface)
+
+if currentMac == options.new_mac:
+    print('[+] MAC address successfully changed to ' + currentMac)
+else:
+    print('[-] MAC address was not changed.')
