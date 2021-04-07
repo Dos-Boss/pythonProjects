1c4f7bbec452d655cdd59d20e6598347cd868e49
Brendan McCann <64688605+Dos-Boss@users.noreply.github.com>
2021-04-07 15:24:13 +0800

    Update addressbook.py
    Added self-destruct and backup functions,
    cleaned up function order.

--- Tafe/Assessment 2 - Address Book/addressbook.py
+++ Tafe/Assessment 2 - Address Book/addressbook.py
@@ -4,10 +4,11 @@
 # 05/04/2021
 
 import pickle
+import shutil
 
 
 fileName = "addrbook.bin"
-
+backupCreated = bool
 
 class Record:
     def __init__(self, name, email, phone, addr):
@@ -30,179 +31,6 @@ def checkExists():
             return True
     except FileNotFoundError:
         x = open(fileName, 'x')
-
-
-# Checks if item being added is first in an array
-# if so, it is added as 'write' (effectively overwriting any existing pickle file)
-# if it's not, then the additional items are appended.
-def addRecord(rec, isFirst=False):
-    if isFirst is True:
-        with open(fileName, 'wb') as outList:
-            pickle.dump(rec, outList, pickle.HIGHEST_PROTOCOL)
-    else:
-        with open(fileName, 'ab') as outList:
-            pickle.dump(rec, outList, pickle.HIGHEST_PROTOCOL)
-    buildIndex()
-    
-
-# Handles adding an array of Records
-def addRecords(recList):
-    isFirst = True
-
-    for rec in recList:
-        if isFirst is True:
-            isFirst = False
-            addRecord(rec, True)
-        else:
-            addRecord(rec)
-
-
-# Creates a list of names from the index file in lowercase for comparisons
-def getLowerList():
-    lowerList = []
-    for x in range(len(index["Name"])):
-        lowerList.append(index["Name"][x].lower())
-    return lowerList
-
-
-def delRecord(name, givenIndex=None):
-    lowerList = getLowerList()
-
-    if givenIndex is None:
-        try:
-            x = lowerList.index(name)
-        except ValueError:
-            print("Name not found!\n")
-    else:
-        x = givenIndex
-
-    if lowerList.count(name) == 1 or givenIndex is not None:
-        del index["Index"][x]
-        del index["Register"][x]
-        del index["Name"][x]
-        del index["Email"][x]
-        del index["Phone"][x]
-        del index["Address"][x]
-
-        return 3
-
-    elif lowerList.count(name) > 1 and givenIndex is None:
-        occLocations = [i for i, x in enumerate(lowerList) if x == name]
-        print(f"\n*** {len(occLocations)} occurances of {name.capitalize()} found! ***\n")
-        with open(fileName, 'rb') as file:
-            for x in range(len(occLocations)):
-                file.seek(index["Register"][occLocations[x]])
-                y = pickle.load(file)
-                print(f"Index {occLocations[x]} | " + y.getInfo() + "\n")
-
-        while True:
-            uInput = input("\n(C)ancel, (A)ll\nPlease enter index number of record you wish to delete: ")
-            if uInput.lower() == "c" or uInput.lower() == "cancel":
-                return
-            elif uInput.lower() == "a" or uInput == "all":
-                registersToDel = []
-                indexToDel = 0
-
-                for x in range(len(occLocations)):
-                    registersToDel.append(index["Register"][occLocations[x]])
-
-                for x in registersToDel:
-                    indexToDel = index["Register"].index(x)
-                    delRecord(index["Name"][indexToDel], indexToDel)
-                return 2
-            else:
-                try:
-                    if int(uInput) in occLocations:
-                        delRecord(name, int(uInput))
-                        return 1
-                    else:
-                        pass
-                except ValueError:
-                    pass
-        
-
-# Wipes index and pickled file
-def selfDestruct():
-    valid = ["y", "n"]
-    confirm = ""
-
-    while confirm not in valid:
-        confirm = input("\nThis will erase ALL data, are you sure? y/n: ").lower()
-
-    if confirm == 'y':
-        global index
-        index.clear()
-        index = {"Index": [],
-                 "Register": [],
-                 "Name": [],
-                 "Email": [],
-                 "Phone": [],
-                 "Address": []}
-        pickleFromIndex()
-        buildIndex()
-        print("\nAll data successfully wiped!")
-    else:
-        return
-
-
-def getRecord(name):
-    lowerList = getLowerList()
-
-    if name.lower() in lowerList:
-        if lowerList.count(name) < 2:
-            x = lowerList.index(name)
-            with open(fileName, 'rb') as file:
-                file.seek(index["Register"][x])
-                y = pickle.load(file)
-                print(f"\nRecord found at index {x}.")
-                print(y.getInfo())
-            return
-        else:
-            occLocations = [i for i, x in enumerate(lowerList) if x == name]
-            print(f"\n*** {len(occLocations)} occurances of {name.capitalize()} found! ***\n")
-            with open(fileName, 'rb') as file:
-                for x in range(len(occLocations)):
-                    file.seek(index["Register"][occLocations[x]])
-                    y = pickle.load(file)
-                    print(f"Index: {occLocations[x]} | " + y.getInfo() + "\n")
-    else:
-        print("\nNo record found.")
-
-
-# Prints a number equal to the amount of items in pickle file
-def recordCount():
-    count = 0
-
-    for item in readFile(fileName):
-        count += 1
-    return count
-
-
-# When iterated through, examines contents of pickle file and adds entry register points to an array.
-def readFile(path):
-    global lstIndex
-    lstIndex = []
-
-    with open(path, 'rb') as file:
-        try:
-            while True:
-                lstIndex.append(file.tell())
-                yield pickle.load(file)
-        except EOFError:
-            pass
-    
-
-def printAllRecords():
-    recordCount()
-
-    with open(fileName, 'rb') as file:
-        for x in range(len(lstIndex)):
-            try:
-                file.seek(lstIndex[x])
-                y = pickle.load(file)
-                print(f"Index: {x} | " + y.getInfo())
-            except EOFError:
-                break
 
 
 # Constructs a dictionary of pickled information
@@ -246,3 +74,250 @@ def pickleFromIndex():
             newRec = Record(name, email, phone, addr)
             toAdd.append(newRec)
         addRecords(toAdd)
+
+
+# When iterated through, examines contents of pickle file and adds entry register points to an array.
+def readFile(path):
+    global lstIndex
+    lstIndex = []
+
+    with open(path, 'rb') as file:
+        try:
+            while True:
+                lstIndex.append(file.tell())
+                yield pickle.load(file)
+        except EOFError:
+            pass
+
+def recordCount():
+    count = 0
+
+    for item in readFile(fileName):
+        count += 1
+    return count
+
+
+def printAllRecords():
+    recordCount()
+
+    with open(fileName, 'rb') as file:
+        for x in range(len(lstIndex)):
+            try:
+                file.seek(lstIndex[x])
+                y = pickle.load(file)
+                print(f"Index: {x} | " + y.getInfo())
+            except EOFError:
+                break
+
+
+def createRecord():
+    print("\n*** Adding Record ***\n")
+    name = input("Please enter name: ").capitalize()
+    email = input("Please enter email address: ")
+    phone = input("Please enter phone number: ")
+    addr = input("Please enter home address: ")
+
+    newRec = Record(name, email, phone, addr)
+    addRecord(newRec)
+
+    print("\n*** Record Successfully Added! ***")
+
+
+# Checks if item being added is first in an array
+# if so, it is added as 'write' (effectively overwriting any existing pickle file)
+# if it's not, then the additional items are appended.
+def addRecord(rec, isFirst=False):
+    if isFirst is True:
+        with open(fileName, 'wb') as outList:
+            pickle.dump(rec, outList, pickle.HIGHEST_PROTOCOL)
+    else:
+        with open(fileName, 'ab') as outList:
+            pickle.dump(rec, outList, pickle.HIGHEST_PROTOCOL)
+    buildIndex()
+    
+
+# Handles adding an array of Records
+def addRecords(recList):
+    isFirst = True
+
+    for rec in recList:
+        if isFirst is True:
+            isFirst = False
+            addRecord(rec, True)
+        else:
+            addRecord(rec)
+
+
+# Creates a list of names from the index file in lowercase for comparisons
+def getLowerList():
+    lowerList = []
+    for x in range(len(index["Name"])):
+        lowerList.append(index["Name"][x].lower())
+    return lowerList
+
+
+def delRecord(name, givenIndex=None):
+    lowerList = getLowerList()
+
+    if givenIndex is None:
+        try:
+            x = lowerList.index(name)
+        except ValueError:
+            print("*** Name not found! ***\n")
+    else:
+        x = givenIndex
+
+    if lowerList.count(name) == 1 or givenIndex is not None:
+        del index["Index"][x]
+        del index["Register"][x]
+        del index["Name"][x]
+        del index["Email"][x]
+        del index["Phone"][x]
+        del index["Address"][x]
+
+        return 1
+
+    elif lowerList.count(name) > 1 and givenIndex is None:
+        foundLocations = [i for i, x in enumerate(lowerList) if x == name]
+        print(f"\n*** {len(foundLocations)} occurances of {name.capitalize()} found! ***\n")
+        with open(fileName, 'rb') as file:
+            for x in range(len(foundLocations)):
+                file.seek(index["Register"][foundLocations[x]])
+                y = pickle.load(file)
+                print(f"Index {foundLocations[x]} | " + y.getInfo() + "\n")
+
+        while True:
+            uInput = input("\n(C)ancel, (A)ll\nPlease enter index number of record you wish to delete: ")
+            if uInput.lower() == "c" or uInput.lower() == "cancel":
+                return
+            elif uInput.lower() == "a" or uInput == "all":
+                registersToDel = []
+                indexToDel = 0
+
+                for x in range(len(foundLocations)):
+                    registersToDel.append(index["Register"][foundLocations[x]])
+
+                for x in registersToDel:
+                    indexToDel = index["Register"].index(x)
+                    delRecord(index["Name"][indexToDel], indexToDel)
+                return 2
+            else:
+                try:
+                    if int(uInput) in foundLocations:
+                        delRecord(name, int(uInput))
+                        return 1
+                    else:
+                        pass
+                except ValueError:
+                    pass
+        
+
+def getRecord(name):
+    lowerList = getLowerList()
+
+    if name.lower() in lowerList:
+        if lowerList.count(name) < 2:
+            x = lowerList.index(name)
+            with open(fileName, 'rb') as file:
+                file.seek(index["Register"][x])
+                y = pickle.load(file)
+                print("\n*** Match found! ***")
+                print(f"\nIndex: {x} | " + y.getInfo() + "\n")
+            return
+        else:
+            foundLocations = [i for i, x in enumerate(lowerList) if x == name]
+            print(f"\n*** {len(foundLocations)} Occurances of {name.capitalize()} Found! ***\n")
+            with open(fileName, 'rb') as file:
+                for x in range(len(foundLocations)):
+                    file.seek(index["Register"][foundLocations[x]])
+                    y = pickle.load(file)
+                    print(f"Index: {foundLocations[x]} | " + y.getInfo() + "\n")
+    else:
+        print("\n*** No matches found ***")
+
+
+# Wipes data from index and pickled file
+def selfDestruct():
+    if recordCount() < 1:
+        print("\n*** Records already clear ***")
+        return
+
+    valid = ["y", "n"]
+    confirm = ""
+    destruct = False
+
+    print("\n*** This will erase ALL data in current address book ***\n")
+
+    while confirm not in valid:
+        confirm = input("Create backup before proceeding? y/n: ").lower()
+
+    if confirm == 'y':
+        if saveBackup():
+            destruct = True
+        else:
+            confirm = ""
+            while confirm not in valid:
+                confirm = input("\nWipe all data anyway? y/n: ").lower()
+                if confirm == 'y':
+                    destruct = True
+                else:
+                    return
+
+    if confirm == 'n':
+        confirm = ""
+        while confirm not in valid:
+            confirm = input("\nWipe all data anyway? y/n: ").lower()
+        if confirm == 'y':
+            destruct = True
+        else:
+            return
+
+    if destruct:
+        global index
+        index.clear()
+        index = {"Index": [],
+                 "Register": [],
+                 "Name": [],
+                 "Email": [],
+                 "Phone": [],
+                 "Address": []}
+        pickleFromIndex()
+        buildIndex()
+        print("\n*** All data successfully wiped! ***")
+
+
+def saveBackup():
+    if recordCount() < 1:
+        print("\n*** No data to save! ***")
+    else:
+        try:
+            open("addrbook.bak")
+        except FileNotFoundError:
+            shutil.copy(fileName, "addrbook.bak")
+            print("\n*** Backup successfully saved! ***")
+            return True
+        else:
+            valid = ["y", "n"]
+            confirm = ""
+
+            while confirm not in valid:
+                confirm = input("\nOverwrite current backup? y/n: ")
+
+            if confirm == 'y':
+                shutil.copy(fileName, "addrbook.bak")
+                print("\n*** Backup successfully saved! ***")
+                return True
+            else:
+                return False
+
+        
+def loadBackup():
+    try:
+        open("addrbook.bak")
+    except FileNotFoundError:
+        print("\n*** No data to backup! ***")
+    else:
+        shutil.copy("addrbook.bak", fileName)
+        buildIndex()
+        print("\n*** Backup successfully restored! ***")
+        return
