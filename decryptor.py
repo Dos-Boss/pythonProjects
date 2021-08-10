9a5e43e522a05f92c1993d1780abbe5478bd4561
Brendan McCann <64688605+Dos-Boss@users.noreply.github.com>
2021-08-11 00:00:47 +0800

    Update decryptor.py

--- decryptor.py
+++ decryptor.py
@@ -23,24 +23,39 @@ def get_args():
     return usr_keys
 
 def init():
-    usr_keys = get_args()
-    priv_key = RSA.importKey(open(usr_keys.priv_key, 'rb').read())
-    cipher_rsa = PKCS1_OAEP.new(priv_key)
-    sec_key = open(usr_keys.sec_key, 'rb').read()
     global session_key
-    session_key = cipher_rsa.decrypt(sec_key).decode()
-    print("Keys Initialised...Get Psyched!\n")
-    available_drives = [chr(x) for x in range(65, 91) if os.path.exists(chr(x) + ":")]
-    print(available_drives)
-    global home
-    home = input("Please Enter Drive Letter to Scan: ").upper() + ":/"
 
+    usr_keys = get_args()
+    private_key = RSA.importKey(open(usr_keys.priv_key, 'rb').read())
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    secret_key = open(usr_keys.sec_key, 'rb').read()
+    session_key = cipher_rsa.decrypt(secret_key).decode()
+    print("Keys Initialised...Get Psyched!\n")
+    drive_to_scan()
+    return
+
+def drive_to_scan():
+    global available_drives
+    global drive
+    drive = []
+    uinput = ""
+
+    get_drives = [chr(x) for x in range(65, 91) if os.path.exists(chr(x) + ":")]
+    get_drives.append("All")
+    while uinput not in get_drives:
+        print(get_drives)
+        uinput = input("Please Enter Drive Letter to Scan: ").capitalize()
+    if uinput != "All":
+        drive.append(uinput)
+    else:
+        drive = get_drives[:-1]
+    return
+    
 def decryptor(in_file):
     ifile = open(in_file, 'rb')
     iv = ifile.read(8)
     cipher_des3 = DES3.new(session_key, DES3.MODE_CFB, iv)
     data = cipher_des3.decrypt(ifile.read())
-    # print(data.decode())
     return data.decode()
 
 def write_file(file, data):
@@ -51,12 +66,15 @@ def write_file(file, data):
 
 def main():
     init()
-    os.chdir(home)
-    for root, dirs, files in os.walk(home):
-        for file in files:
-            if file.endswith(".cry"):
-                fname = os.path.abspath(os.path.join(root, file))
-                write_file(fname, decryptor(fname))
-                print(fname + " - Decrypted Successfully")
+    print("\nPlease Wait...")
+    for x in range(len(drive)):
+        os.chdir(drive[x] + ":/")
+        for root, dirs, files in os.walk(drive[x] + ":/"):
+            for file in files:
+                if file.endswith(".cry"):
+                    fname = os.path.abspath(os.path.join(root, file))
+                    write_file(fname, decryptor(fname))
+                    print(fname + " - Decrypted Successfully")
+    print("Decryption Complete")
             
 main()
